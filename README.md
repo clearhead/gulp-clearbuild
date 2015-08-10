503f1786d91c93a9962904aee6d495579647e70a
Tom Fuertes <tomfuertes@gmail.com>
2015-08-10 17:29:48 -0500

    init.sh

--- README.md
+++ README.md
@@ -8,7 +8,15 @@ Clearhead Gulp Extension
 ## Usage
 
 ```js
+/*jshint unused:false*/
 import _gulp from 'gulp';
 import clearbuild from 'gulp-clearbuild';
 const gulp = clearbuild(_gulp);
+```
+
+OR as a helper function:
+
+```bash
+# before: mkdir exp-name && cd exp-name
+bash <(curl -s https://raw.githubusercontent.com/clearhead/gulp-clearbuild/master/init.sh)
 ```
---
+++ init.sh
@@ -0,0 +1,35 @@
+#!/usr/local/env bash
+
+touch gulpfile.babel.js
+
+echo "/*jshint unused:false*/" >> gulpfile.babel.js
+echo "import _gulp from 'gulp';" >> gulpfile.babel.js
+echo "import clearbuild from 'gulp-clearbuild';" >> gulpfile.babel.js
+echo "const gulp = clearbuild(_gulp);" >> gulpfile.babel.js
+
+mkdir src
+
+touch src/v1.html
+echo '<div id="expx">Hello World</div>' >> src/v1.html
+touch src/v1.scss
+echo "#expx{foo:bar;}" >> src/v1.scss
+touch src/v1.js
+echo "import html from './v1.html';" >> src/v1.js
+echo "import css from './v1.scss';" >> src/v1.js
+echo "\$('body').append(html);" >> src/v1.js
+
+echo "
+
+SUCCESS!!! Now run
+
+  npm outdated
+
+to see if you have any outdated required packages in:
+ - gulp-clearbuild
+ - clearhead
+ - jsxr
+
+If any of the above are outdated, cd into clearhead-experiments
+  and then run npm install
+
+"
