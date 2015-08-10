c02b07d450c5ca4e6abc6d802c10369ac99b273e
Casey White <casey.r.white@gmail.com>
2015-08-09 21:44:32 -0500

    add css and npi to testing

--- test/gulpfile.babel.js
+++ test/gulpfile.babel.js
@@ -1,6 +1,3 @@
 import _gulp from 'gulp';
-import gulpHelp from 'gulp-help';
-const gulp = gulpHelp(_gulp);
-
 import clearbuild from '../src/index';
-clearbuild.use(gulp);
+const gulp = clearbuild(_gulp);
--- test/src/v1.js
+++ test/src/v1.js
@@ -1,12 +1,17 @@
 /* optimizely_evaluate=force */
 import { logger } from './mods/utils';
+import jsxr from 'jsxr';
 
-const test = 'test';
+const test = '**********\nclearbuild test2\n**********';
 logger(test);
 
 $.exp = $.exp || {};
 
-$.exp.elem = '<div>Testing 123</div>';
+$.exp.elem = (
+  <div className="exp-container warn">
+    <p className="msg">Test 1 of the clearbuild system.</p>
+  </div>
+);
 
 /* optimizely_evaluate=safe */
-$('body').append($.exp.elem);
+$('body').prepend($.exp.elem);
