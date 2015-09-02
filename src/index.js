b5c2bbef17628ec43865f588bd3da68b36a4ebab
Casey White <casey.r.white@gmail.com>
2015-09-01 21:10:15 -0500

    update gulp export

--- src/index.js
+++ src/index.js
@@ -26,76 +26,79 @@ const paths = {
 const gulp = gulpHelp(_gulp);
 const sequence = gulpSeq.use(gulp);
 
-gulp.task('default', 'Run the dev task.', ['dev']);
+export default (() => {
 
-// -- Live Development ----------
-gulp.task('dev', 'Build and preview your experiment.', () => {
-  return sequence(
-    ['lint:scripts'/*, 'lint:stylesheets'*/],
-    ['build:scripts', 'build:stylesheets'],
-    'npi',
-    'watch'
-  )();
-});
+  gulp.task('default', 'Run the dev task.', ['dev']);
 
-gulp.task('npi', 'Start new NPI process.', () => {
-  return sequence('npi:kill', 'npi:start')();
-});
+  // -- Live Development ----------
+  gulp.task('dev', 'Build and preview your experiment.', () => {
+    return sequence(
+      ['lint:scripts'/*, 'lint:stylesheets'*/],
+      ['build:scripts', 'build:stylesheets'],
+      'npi',
+      'watch'
+    )();
+  });
 
-gulp.task('npi:start', 'Start NPI process.', shell.task('npi'));
+  gulp.task('npi', 'Start new NPI process.', () => {
+    return sequence('npi:kill', 'npi:start')();
+  });
 
-gulp.task('npi:kill', 'Kill NPI process.', () => {
-  return finder.find(8000, function(err, pids) {
-    pids.forEach((pid) => {
-      shell.task(`kill -9 ${ pid }`)();
+  gulp.task('npi:start', 'Start NPI process.', shell.task('npi'));
+
+  gulp.task('npi:kill', 'Kill NPI process.', () => {
+    return finder.find(8000, function(err, pids) {
+      pids.forEach((pid) => {
+        shell.task(`kill -9 ${ pid }`)();
+      });
     });
   });
-});
 
-gulp.task('watch', 'Rebuild when experiment files change.', () => {
-  gulp.watch(paths.src.html, ['build']);
-  gulp.watch(paths.src.scripts, ['build']);
-  gulp.watch(paths.src.stylesheets, ['build']);
-});
+  gulp.task('watch', 'Rebuild when experiment files change.', () => {
+    gulp.watch(paths.src.html, ['build']);
+    gulp.watch(paths.src.scripts, ['build']);
+    gulp.watch(paths.src.stylesheets, ['build']);
+  });
 
-// -- Build Experiment ----------
-gulp.task('build', 'Build experiment scripts and stylesheets.', () => {
-  return sequence('lint', 'build:clean', ['build:scripts', 'build:stylesheets'])();
-});
+  // -- Build Experiment ----------
+  gulp.task('build', 'Build experiment scripts and stylesheets.', () => {
+    return sequence('lint', 'build:clean', ['build:scripts', 'build:stylesheets'])();
+  });
 
-gulp.task('build:clean', 'Clean build.', (cb) => {
-  return del(paths.dest, cb);
-});
+  gulp.task('build:clean', 'Clean build.', (cb) => {
+    return del(paths.dest, cb);
+  });
 
-gulp.task('build:scripts', 'Transpile and browserify scripts.', () => {
-  return gulp.src(paths.scripts)
-    .pipe(bundlify())
-    .pipe(gulp.dest(paths.dest));
-});
+  gulp.task('build:scripts', 'Transpile and browserify scripts.', () => {
+    return gulp.src(paths.scripts)
+      .pipe(bundlify())
+      .pipe(gulp.dest(paths.dest));
+  });
 
-gulp.task('build:stylesheets', 'Compile stylesheets.', () => {
-  return gulp.src(paths.stylesheets)
-    .pipe(gulpSass().on('error', gulpSass.logError))
-    .pipe(gulp.dest(paths.dest));
-});
+  gulp.task('build:stylesheets', 'Compile stylesheets.', () => {
+    return gulp.src(paths.stylesheets)
+      .pipe(gulpSass().on('error', gulpSass.logError))
+      .pipe(gulp.dest(paths.dest));
+  });
 
-// -- Lint Experiment ----------
-gulp.task('lint',
-  'Lint scripts and stylesheets',
-  ['lint:scripts'/*, 'lint:stylesheets'*/]
-);
+  // -- Lint Experiment ----------
+  gulp.task('lint',
+    'Lint scripts and stylesheets',
+    ['lint:scripts'/*, 'lint:stylesheets'*/]
+  );
 
-gulp.task('lint:scripts', 'Lint scripts.', () => {
-  return gulp.src(paths.scripts)
-    .pipe(eslint(eslintConfig))
-    .pipe(eslint.format());
-});
+  gulp.task('lint:scripts', 'Lint scripts.', () => {
+    return gulp.src(paths.scripts)
+      .pipe(eslint(eslintConfig))
+      .pipe(eslint.format());
+  });
 
-gulp.task('lint:stylesheets', 'Compile and lint stylesheets.', () => {
-  const task = gulp.src(paths.stylesheets)
-    .pipe(gulpSass().on('error', gulpSass.logError))
-    .pipe(csslint(csslintConfig))
-    .pipe(csslint.reporter());
-});
+  gulp.task('lint:stylesheets', 'Compile and lint stylesheets.', () => {
+    const task = gulp.src(paths.stylesheets)
+      .pipe(gulpSass().on('error', gulpSass.logError))
+      .pipe(csslint(csslintConfig))
+      .pipe(csslint.reporter());
+  });
 
-export default gulp;
+  return gulp;
+})();
