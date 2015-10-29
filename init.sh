23ae9b230d5267a4da52acbe975c160513cf2e90
beaulm <beaulm@gmail.com>
2015-10-29 13:16:13 -0500

    Adding stylesheet to the head is now instant

--- init.sh
+++ init.sh
@@ -15,9 +15,9 @@ touch src/v1.js
 echo "/* _optimizely_evaluate=force */ /*global \$*/" >> src/v1.js
 echo "import html from './v1.html';" >> src/v1.js
 echo "import css from './v1.scss';" >> src/v1.js
-echo "window.expX = { html, css };" >> src/v1.js
+echo "window.expX = { html };" >> src/v1.js
+echo "\$('head').append('<style>'+css+'</style>');" >> src/v1.js
 echo "/* _optimizely_evaluate=safe */" >> src/v1.js
-echo "\$('head').append('<style>'+expX.css+'</style>');" >> src/v1.js
 echo "\$('body').append(expX.html);" >> src/v1.js
 
 echo "
